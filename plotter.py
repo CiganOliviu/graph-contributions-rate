49668509a3abaaaf3ffb9d5a36e34065ac968ad6
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-05-16 23:15:41 +0300

    Create other plotters

--- plotter.py
+++ plotter.py
@@ -2,6 +2,6 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
 plt.plot([919, 921, 935, 964, 973, 1000, 1012, 1014, 1023, 1076, 1090], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
-# plt.plot([], [])
+# plt.plot([8.014705882352942], [1])
 
 plt.show()
---
+++ rates_plotter.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+plt.plot([0, 8.014705882352942], [0, 1])
+
+plt.show()
