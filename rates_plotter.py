3dd994e16f452a14e484bfcfe00bc033e4dbea85
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-05-21 23:35:44 +0300

    Create plotters

--- rates_plotter.py
+++ rates_plotter.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
-plt.plot([0, 8.014705882352942, 8.18840579710145], [0, 1, 2])
+plt.plot([0, 8.014705882352942, 8.18840579710145, 8.184397163120567],
+         [0, 1, 2, 3])
 
 plt.show()
