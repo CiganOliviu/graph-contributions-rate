5e27cb1d22a6462d4c568c6d0f1d3de7bf20ab11
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-18 21:39:05 +0300

    Update predicted_plotter.py

--- predicted_plotter.py
+++ predicted_plotter.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
-plt.plot([0, 3005.2395209580836, 2996.041666666667],
-         [0, 1, 2]
+plt.plot([0, 3005.2395209580836, 2996.041666666667, 3027.9881656804732],
+	
+         [0, 1, 2, 3]
          )
 
 plt.show()
