165e7b59d7b3cd7b4654a10075bbe7047c7959b7
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-30 00:13:32 +0300

    Update contributions_plotter.py

--- contributions_plotter.py
+++ contributions_plotter.py
@@ -1,13 +1,5 @@
+from data_source import contributions, contributions_indexes
 from plotter import plot_based_on_data_and_indexes
 
-contributions = [919, 921, 935, 964, 973, 1000, 1012, 1014, 1023, 1076,
-		  1090, 1130, 1145, 1155, 1167, 1194, 1237, 1278, 1302, 
-		  1320, 1330, 1341, 1350, 1357, 1365, 1375, 1383, 1402, 1414, 1429, 1451,
-		  1458, 1613, 1638, 1735, 1760] 
 
-indexes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-          17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-          31, 32, 33, 34, 35, 36]
-
-
-plot_based_on_data_and_indexes(contributions, indexes)
+plot_based_on_data_and_indexes(contributions, contributions_indexes)
