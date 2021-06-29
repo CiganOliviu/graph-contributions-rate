7a5487db1b1970a5e2a6a4da9c10a8241cb1e37a
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-30 00:04:55 +0300

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -1,5 +1,16 @@
-def plot_based_on_index_and_data(indexes, data_set_one):
+import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
+
+def plot_based_on_indexes_and_data(indexes, data_set_one):
 
 	plt.plot(indexes, data_set_one)
+
+	plt.show()
+
+
+def plot_based_on_data_and_indexes(data_set_one, indexes):
+
+	plt.plot(data_set_one, indexes)
 
 	plt.show()
