3b0317d083feac685a2768f681801867f0e652d7
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-30 00:05:02 +0300

    Update predicted_plotter.py

--- predicted_plotter.py
+++ predicted_plotter.py
@@ -1,7 +1,4 @@
-from plotter import plot_based_on_data
-import matplotlib.pyplot as plt
-
-plt.style.use('ggplot')
+from plotter import plot_based_on_indexes_and_data
 
 indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
 
@@ -9,6 +6,4 @@ predictions = [3005.2395209580836, 2996.041666666667, 3027.9881656804732, 3018.1
           	   3061.35838150289, 3040.9714285714285, 3345.142045454545, 3358.8202247191007, 3537.8491620111736,
                3568.888888888889]
 
-plt.plot(indexes, predictions)
-
-plt.show()
+plot_based_on_indexes_and_data(indexes, predictions)
