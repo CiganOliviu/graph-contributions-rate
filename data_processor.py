fad39c33872365b2bd84c175b50b12c29347dfe4
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-30 00:15:29 +0300

    Update data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -1,3 +1,10 @@
-from contributions_plotter import contributions
-from predicted_plotter import predictions
-from rates_plotter import rates
+from data_source import contributions
+from data_source import predictions
+from data_source import rates
+
+print(contributions)
+print('======================')
+print(predictions)
+print('======================')
+print(rates)
+print('======================')
